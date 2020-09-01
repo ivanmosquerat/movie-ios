e215cce33a68286e7dbb3e5bab20c9f13ce2bc84
Ivan Mosquera <ivanmosquerat13@gmail.com>
2020-09-02 01:22:27 +1000

    Estructura de carpetas creada, tab bar controller terminado, storyboards reference creados.

---
+++ movie-app/Modules/Profile/ProfileViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ProfileViewController.swift
+//  movie-app
+//
+//  Created by Ivan Mosquera on 2/9/20.
+//  Copyright © 2020 Ivan Mosquera. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
